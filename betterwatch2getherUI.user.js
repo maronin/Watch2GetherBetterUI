572097eb3d3c676c26388fe7a32d05b3e0059145
Mark Aronin <steroidcat@gmail.com>
2016-06-18 21:31:31 -0400

    Initial commit with the code

---
+++ betterwatch2getherUI.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         watch2gether better player
+// @version      1.0.0
+// @description  Makes the UI way better.
+// @author       Mark
+// @match        http://www.watch2gether.com/rooms/*
+// @match        https://www.watch2gether.com/rooms/*
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @require      https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+if( typeof GM_addStyle != 'function' )
+    function GM_addStyle(css)
+{
+    var style = document.createElement('style');
+    style.innerHTML = css;
+    style.type='text/css';
+    document.getElementsByTagName('head')[0].appendChild(style);
+}
+    var minifiedCss = "#player-chat { position: relative !important; } #userbar-chat { height: 100% !important; } .chat-right {position: fixed !important; bottom: 0; right: 0; height: 100%; width: 380px !important; background: white; padding: 0px; } .ui.container { width: calc(100% - 380px); margin-left: 1px !important; margin-right: auto!important; } #playlist-history .bottom.attached.segment { height: calc(100% - 40px);} ";
+GM_addStyle(minifiedCss);
+
+$(document).ready(function() {
+
+    var chat = $("#userbar-chat").parent();
+    var playlist = $("#playlist-history").parent();
+    var playerChat = $("#player-chat");
+    var playerSearch = $($("#client-container .ui").children()[2]);
+    var searchResultMenu = $("#search-results-menu").parent();
+
+    chat.removeClass("column");
+    chat.addClass("chat-right");
+    $("#client-pusher").append(chat);
+    playerSearch.prepend(chat);
+    playerChat.append(playlist);
+
+    playlist.removeClass('five');
+    prependClass(playlist, 'six');
+
+    searchResultMenu.removeClass('eleven');
+    prependClass(searchResultMenu, 'sixteen');
+
+
+    $('#playlist-history').css('height', '100%');
+    $('#client-container').css('padding-bottom', '0px');
+
+    //Remove the like button.
+    $('.like-button').remove();
+    //Remove the feedback sticker.
+    $('.feedback-sticker').remove();
+    //remove the footer.
+    $('.footer').remove();
+});
+
+function prependClass(ele, className) {
+    var classes = ele.attr('class');
+    classes = className + ' ' + classes;
+    ele.attr('class', classes);
+}
